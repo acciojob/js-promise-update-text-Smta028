a9d91a981550f8613c56bdd73ca2e171029ef2f0
Samta <108336676+Smta028@users.noreply.github.com>
2023-08-01 12:21:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your JS code here. If required.
 const output= document.getElementById("output");
+return new Promise((resolve,reject)=>{
+	resolve=>setTimeout(()=>{
+		output.innerHTML="Hello,world!"
+	},1000)
+})
 
-setTimeout(()=>{
-	output.innerHTML="Hello,world!"
-},1000)
